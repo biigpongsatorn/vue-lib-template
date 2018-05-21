8f2fc1c09c5330b511d725675087fbe3ee3b87ac
biig_pongsatorn <biig_pongsatorn@hotmail.com>
2018-05-21 11:39:29 +0700

    📦  set template

---
+++ template/demo/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <{{ name }}/>
+  </div>
+</template>
+
+<script>
+import {{ name }} from '../src/index.vue'
+
+export default {
+  name: 'app',
+  components: { {{ name }} }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ template/src/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Hello world, Welcome to my component 👏🏻 
+  </div>
+</template>
+
+<script>
+export default {
+  name: '{{ name }}',
+}
+</script>
+
+<style scoped>
+</style>
